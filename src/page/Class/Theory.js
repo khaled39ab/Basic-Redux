a691b525408483603b84a632f9564057e4fd93d4
khaled39ab <khaled39ab@gmail.com>
2022-10-22 23:30:55 +0600

    common terms related to redux

--- src/page/Class/Theory.js
+++ src/page/Class/Theory.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Theory = () => {
     return (
         <div style={{ 'margin': '10px', 'border': '5px solid magenta', 'padding': '10px' }}>
+
             <div>
                 <h1 style={{ 'textAlign': 'center' }}>What is Redux?</h1>
                 <p>
@@ -11,14 +12,13 @@ const Theory = () => {
                         Redux is a predictable state container for JavaScript apps.
                         <br />
                         It helps you write applications that behave consistently, run in different environments (client, server, and native), and are easy to test. On top of that, it provides a great developer experience, such as live code editing combined with a time traveling debugger.
-                    </q>
-                    <br /> <br />
+                    </q> <br /> <br />
 
                     <strong>Normally:</strong> <br />
                     Redux is a small JS Library. It's for managing medium/large amount of states globally in your app.
                 </p>
-
             </div>
+
             <div>
                 <h1 style={{ 'textAlign': 'center' }}>Why Redux?</h1>
                 <p>
@@ -30,6 +30,7 @@ const Theory = () => {
                     </ul>
                 </p>
             </div>
+
             <div>
                 <h1 style={{ 'textAlign': 'center' }}>When to use Redux?</h1>
                 <p>
@@ -40,6 +41,17 @@ const Theory = () => {
                         <li>When multiple components need to access the same application state.</li>
                         <li>When you're working on a large application with several other people.</li>
                         <li>When application state is updated frequently.</li>
+                    </ul>
+                </p>
+            </div>
+
+            <div>
+                <h3 style={{ 'textAlign': 'center' }}>Some common terms related to redux</h3>
+                <p>
+                    <ul>
+                        <li><strong>React-redux:</strong> redux is used with some common packages such as react-redux.</li>
+                        <li><strong>Redux-toolkit: </strong> recommended way to write redux logic for building redux app easily and avoiding mistakes.</li>
+                        <li><strong>Redux devtools extension:</strong> helps to debug redux app easily.</li>
                     </ul>
                 </p>
             </div>
