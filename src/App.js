70cfda98eee9d9b7bc732c64ce690e47a0be9e36
khaled39ab <khaled39ab@gmail.com>
2022-10-25 21:46:46 +0600

    example of reducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Theory from './page/Class/Theory';
 import Fundametal from './primaryRedux/Fundametal';
+import MultipleReducer from './primaryRedux/MultipleReducer';
 
 function App() {
   return (
     <div>
       {/* <Theory></Theory> */}
-      <Fundametal></Fundametal>
+      {/* <Fundametal></Fundametal> */}
+      <MultipleReducer></MultipleReducer>
     </div>
   );
 }
--- src/primaryRedux/MultipleReducer.js
+++ src/primaryRedux/MultipleReducer.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
 const MultipleReducer = () => {
+    const ADD_PRODUCT = 'ADD_PRODUCT';
+
+    const { createStore } = require('redux');
+
+    const initialProducts = {
+        products: ['Rice', 'Sugar'],
+        items: 2
+    }
+    const addProduct = (product) => {
+        return {
+            type: ADD_PRODUCT,
+            payload: product
+        }
+    }
+
+    const productsReducer = (state = initialProducts, action) => {
+        switch (action.type) {
+            case ADD_PRODUCT:
+                return {
+                    products: [...state.products, action.payload],
+                    items: state.items + 1
+                }
+
+            default:
+                return state;
+        }
+    }
+
+    const store = createStore(productsReducer);
+
+    store.subscribe(() =>{
+        console.log(store.getState());
+    })
+
+    store.dispatch(addProduct('Salt'))
+
     return (
         <div>
-            
+
         </div>
     );
 };
