bd98d8edefe5f955f88a94fe77285913d34d2efe
khaled39ab <khaled39ab@gmail.com>
2022-12-12 22:51:15 +0600

    dispatch fetchData

--- src/page/todos/Todos.js
+++ src/page/todos/Todos.js
@@ -1,11 +1,17 @@
+import axios from 'axios';
 import React from 'react';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk'
 
 const Todos = () => {
+
+    const { createStore, applyMiddleware } = require('redux');
 
     //constants
     const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST';
     const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
     const GET_TODOS_FAILED = 'GET_TODOS_FAILED';
+    const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
 
     //state
     const initialTodos = {
@@ -61,7 +67,31 @@ const Todos = () => {
             default:
                 return state;
         }
+    };
+
+    const fetchData = () => {
+        return (dispatch) => {
+            dispatch(getTodosRequest());
+            axios.get(TODOS_URL)
+                .then(res => {
+                    const todos = res.data;
+                    const title = todos.map(todo => todo.title);
+                    dispatch(getTodosSuccess(title))
+                })
+                .catch(err => {
+                    const errorMessage = err.message;
+                    dispatch(getTodosFailed(errorMessage));
+                })
+        }
     }
+
+    const store = createStore(todosReducer, applyMiddleware(thunk, logger));
+
+    store.subscribe(() => {
+        console.log(store.getState());
+    });
+
+    store.dispatch(fetchData())
 
     return (
         <div>
