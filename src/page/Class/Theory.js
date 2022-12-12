649ce27184e1f929e24d871497e4004fb2ee1cc0
khaled39ab <khaled39ab@gmail.com>
2022-12-12 11:52:39 +0600

    handling warning

--- src/page/Class/Theory.js
+++ src/page/Class/Theory.js
@@ -7,7 +7,7 @@ const Theory = () => {
 
             <div>
                 <h1 style={{ 'textAlign': 'center' }}>What is Redux?</h1>
-                <p>
+                <div>
                     <strong>In Redux web says: </strong> <br />
                     <q>
                         Redux is a predictable state container for JavaScript apps.
@@ -17,24 +17,24 @@ const Theory = () => {
 
                     <strong>Normally:</strong> <br />
                     Redux is a small JS Library. It's for managing medium/large amount of states globally in our app.
-                </p>
+                </div>
             </div>
 
             <div>
                 <h1 style={{ 'textAlign': 'center' }}>Why Redux?</h1>
-                <p>
+                <div>
                     <strong>Redux: Three principles</strong>
                     <ul>
                         <li>The store of the whole application is stored in an object tree within a single store.</li>
                         <li>The only way to change state is to emit an action, an object describing what happened.</li>
                         <li>To specify how the state tree is transformed by action, you write pure reducers.</li>
                     </ul>
-                </p>
+                </div>
             </div>
 
             <div>
                 <h1 style={{ 'textAlign': 'center' }}>When to use Redux?</h1>
-                <p>
+                <div>
                     Not all apps need Redux. It's important to understand the kind of application we're building, the kinds of problems that you need to solve, and what tools can best solve the problems you're facing. <br /> <br />
 
                     <strong>Here are some scenarios when Redux might be the best solution: </strong>
@@ -43,31 +43,31 @@ const Theory = () => {
                         <li>When you're working on a large application with several other people.</li>
                         <li>When application state is updated frequently.</li>
                     </ul>
-                </p>
+                </div>
             </div>
 
             <div>
                 <h3 style={{ 'textAlign': 'center' }}>Some common terms related to redux</h3>
-                <p>
+                <div>
                     <ul>
                         <li><strong>React-redux:</strong> redux is used with some common packages such as react-redux.</li>
                         <li><strong>Redux-toolkit: </strong> recommended way to write redux logic for building redux app easily and avoiding mistakes.</li>
                         <li><strong>Redux devtools extension:</strong> helps to debug redux app easily.</li>
                     </ul>
-                </p>
+                </div>
             </div>
 
             <div>
                 <h1 style={{ 'textAlign': 'center' }}>How Redux works?</h1>
-                <p>
+                <div>
                     <ol>
                         <li>Define state.</li>
                         <li>Dispatch an Action.</li>
                         <li>Reducer update state based on Action Type.</li>
                         <li>store will update the view</li>
                     </ol>
-                    <img style={{'width': "50%"}} src={reduxWork} alt="" />
-                </p>
+                    <img style={{ 'width': "50%" }} src={reduxWork} alt="" />
+                </div>
             </div>
         </div>
     );
