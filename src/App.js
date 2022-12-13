490b77fd201c76c51d137f04ce90375e6aa3b2fd
khaled39ab <khaled39ab@gmail.com>
2022-12-13 23:51:20 +0600

    starting counter app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Theory from './page/Class/Theory';
+import Counter from './page/counter_app/Counter';
 import Home from './page/home/Home';
 import Navbar from './page/navbar/Navbar';
 import Todos from './page/todos/Todos';
@@ -15,8 +16,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/theory' element={<Theory />}></Route>
-        <Route path='/counter-app' element={<FundamentalAgain />}></Route>
-        <Route path='/todos' element={<Todos/>}></Route>
+        <Route path='/counter-app' element={<Counter />}></Route>
+        <Route path='/todos' element={<Todos />}></Route>
       </Routes>
     </div>
   );
