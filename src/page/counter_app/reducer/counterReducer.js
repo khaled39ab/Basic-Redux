7fdb7aaec783124e8ec3fa38734505096627fe6e
khaled39ab <khaled39ab@gmail.com>
2022-12-14 11:32:33 +0600

    create and providing store

--- src/page/counter_app/reducer/counterReducer.js
+++ src/page/counter_app/reducer/counterReducer.js
@@ -8,14 +8,17 @@ const counterReducer = (state = initialCounter, action) => {
     switch (action.type) {
         case INCREMENT:
             return {
+                ...state,
                 count: state.count + 1
             }
         case DECREMENT:
             return {
+                ...state,
                 count: state.count - 1
             }
         case RESET:
             return {
+                ...state,
                 count: 0
             }
 
