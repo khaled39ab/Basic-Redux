ac5d143226619dabe4d9abb193f6134d1006e071
khaled39ab <khaled39ab@gmail.com>
2022-12-19 22:27:37 +0600

    added handle button

--- src/page/counter_app/Counter.js
+++ src/page/counter_app/Counter.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const Counter = () => {
+    const handleDecrement = () => {
+
+    }
+    const handleReset = () => {
+
+    }
+    const handleIncrement = () => {
+
+    }
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
             <h1>Counter App</h1>
-        </div>
+
+            <h3>Count: { }</h3>
+            <button onClick={handleDecrement}>Decrement</button>
+            <button onClick={handleReset}>Reset</button>
+            <button onClick={handleIncrement}>Increment</button>
+        </div >
     );
 };
 
