c7310ea24f73feb47a652311a8d19e51892a0176
khaled39ab <khaled39ab@gmail.com>
2022-12-20 23:51:56 +0600

    todos action done

---
+++ src/page/todos_App/todos_action/todosAction.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+import { TODOS_FAILED, TODOS_REQUEST, TODOS_SUCCESS } from "../todos_constants/todosConstants"
+
+const getTodos = () => async dispatch => {
+    dispatch({ type: TODOS_REQUEST })
+
+    try {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/todos')
+
+        dispatch({ type: TODOS_SUCCESS, payload: res.data })
+    } catch (error) {
+        dispatch({ type: TODOS_FAILED, payload: error.message })
+    }
+}
+
+export default getTodos;
