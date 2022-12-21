cb8f5a9412352959945036c5aa027dd14951806e
khaled39ab <khaled39ab@gmail.com>
2022-12-21 07:17:34 +0600

    todos app styled

--- src/page/todos_App/TodosApp.js
+++ src/page/todos_App/TodosApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import getTodos from './todos_action/todosAction';
+import './TodosApp.css'
 
 const TodosApp = () => {
     const { isLoading, todos, error } = useSelector(state => state);
@@ -10,11 +11,11 @@ const TodosApp = () => {
 
     useEffect(() => {
         dispatch(getTodos())
-    }, []);
+    }, [dispatch]);
 
     return (
         <div>
-            <h1>Todos App</h1>
+            <h1 className='todos-title'>Todos App</h1>
             {isLoading && <h3>Loading ...</h3>}
             {error && <h3>{error.message}</h3>}
             <section>
@@ -23,7 +24,7 @@ const TodosApp = () => {
                         return (
                             <article key={todo.id}>
                                 <h4>{todo.id}</h4>
-                                <p>{todo.title}</p>
+                                <h4>{todo.title}</h4>
                             </article>
                         );
                     })}
